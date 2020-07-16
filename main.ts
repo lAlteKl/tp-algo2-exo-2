c343f1fed64fb44b2812d9a07d055f8394db5356
lAlteKl <68253525+lAlteKl@users.noreply.github.com>
2020-07-17 09:51:23 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,38 @@
+input.onButtonPressed(Button.A, function () {
+    led.unplot(Colonne, Ligne)
+    Direction_X = -1
+    Colonne += Direction_X
+    if (Colonne > 0) {
+        Colonne = 4
+    }
+    led.plot(Colonne, Ligne)
+})
+input.onButtonPressed(Button.B, function () {
+    led.unplot(Colonne, Ligne)
+    Direction_X = 1
+    Colonne += Direction_X
+    if (Colonne >= 5) {
+        Colonne = 0
+    }
+    led.plot(Colonne, Ligne)
+})
+let Direction_X = 0
+let Direction_y = 0
+let Ligne = 0
+let Colonne = 0
+Colonne = 0
+Ligne = 0
+Direction_y += 1
+led.plot(Colonne, Ligne)
+basic.forever(function () {
+    basic.pause(100)
+    led.unplot(Colonne, Ligne)
+    Ligne += Direction_y
+    led.plot(Colonne, Ligne)
+    if (Ligne == 4) {
+        Direction_y = -1
+    }
+    if (Ligne == 0) {
+        Direction_y = 1
+    }
+})
